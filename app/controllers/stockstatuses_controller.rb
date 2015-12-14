3466086c16f6c4d1cc9b9fd7970460c194e58f66
bcoleman4 <bcoleman4@hotmail.co.uk>
2015-12-14 18:30:35 +0000

    fifth commit

--- app/controllers/stockstatuses_controller.rb
+++ app/controllers/stockstatuses_controller.rb
@@ -1,5 +1,5 @@
 class StockstatusesController < ApplicationController
-  before_action :set_stockstatus, only: [:show, :edit, :update, :destroy, :response]
+  before_action :set_stockstatus, only: [:show, :edit, :update, :destroy, :stocksresponse, :json, :symbols, :ok]
 
   
   def apistatus
@@ -38,10 +38,10 @@ class StockstatusesController < ApplicationController
       end
 
       begin
-        @stocksresponse = HTTParty.get("https://api.stockfighter.io/ob/api/venues/RYME/stocks", timeout: 1)
+        @stocksresponse = HTTParty.get("https://api.stockfighter.io/ob/api/venues/TESTEX/stocks", timeout: 1)
       rescue Net::ReadTimeout
         @stocksresponse = "Venue Timed Out!"
-        @json = response.parsed_response
+        @json = @stocksresponse.parsed_response
         
         puts @json[:ok]
 
